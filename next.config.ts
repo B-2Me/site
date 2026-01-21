d9fc9a6ea8f6dc991efd879ac2468a1861a6e2d5
Nathan R <moso.nate.r@gmail.com>
2026-01-21 09:41:49 -0800

    Fix next config to expect a repository name in production for now

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = '/site'; // <--- MUST MATCH YOUR GITHUB REPO NAME EXACTLY
+
 const nextConfig: NextConfig = {
-  output: "export", // Required for GitHub Pages
-  images: { 
-    unoptimized: true // GitHub Pages cannot resize images on the fly
+  output: "export",
+  basePath: isProd ? repoName : '',
+  assetPrefix: isProd ? repoName : '',
+  images: {
+    unoptimized: true,
   },
 };
 
